f38880ee12969f25084ac1b2496d6b1316c50da1
maofeng <maofeng@maofengtekiMacBook-Air.local>
2017-05-21 16:05:48 +0800

    demo

--- src/main/scala/SparkRiak.scala
+++ src/main/scala/SparkRiak.scala
@@ -13,54 +13,20 @@ object SparkRiak {
         val conf = new SparkConf()
         .setAppName(appName)
         .set("spark.riak.connection.host", "127.0.0.1:8087")
-        // .set("spark.riak.connection.host", "127.0.0.1")
         .set("spark.riak.connections.min", "20")
         .set("spark.riak.connections.max", "50")
 
-        
         val sc = new SparkContext("spark://127.0.0.1:7077", appName, conf)
-        // val sc = new SparkContext(conf)
+        
+        val rdd = sc.riakBucket[String]("bucket1").queryAll()
+        rdd.collect().foreach(println)
 
-        // com.basho.riak.spark.rdd.connector.RiakConnector
-
-        // val kv_bucket_name = "bucket1"
-        // val rdd = sc.riakBucket[String]("bucket1").queryAll()
-        // rdd.collect().foreach(println)
-
-        // val rdd = sc.riakBucket[String]("bucket1").queryAll()
-
-        // rdd.map(_.toUpperCase).saveToRiak("new_bucket")
         println("spark riak!!!")
 
-        // val rdd = sc.riakBucket[String]("bucket1").queryAll()
-        // rdd.map(_.toUpperCase).saveToRiak("new_bucket")
 
-
-        // val rdd = sc.riakBucket[String]("bucket1").queryAll()
-        // rdd.collect().foreach(println)
-
-        val data = Array(1, 2, 3, 4, 5)
-        val testRDD = sc.parallelize(data)
-        testRDD.saveToRiak("kv_bucket_a")
-
-
-
-        // val rdd = sc.riakBucket[String]("bucket1").queryAll()
-        // rdd.map(_.toUpperCase).saveToRiak("new_bucket")
-
-
-
-        // println(data)
-
-        // val rdd = sc.riakBucket[String](kv_bucket_name).queryBucketKeys("Alice", "Bob", "Charlie")
-        // val rdd = sc.riakBucket[String](kv_bucket_name).query2iRange("myIndex", 1L, 5000L)
-        // val rdd = sc.riakBucket[String](kv_bucket_name).partitionBy2iRanges("myIndex", 1->3, 4->6, 7->12)
-        // val rdd = sc.riakBucket[String](kv_bucket_name).query2iKeys("dailyDataIndx", "mon_data", "wed_data", "fri_data")
-
-        // val output_kv_bucket = "test-bucket"
-        // rdd.saveToRiak(output_kv_bucket)
-
-
+        // val data = Array(1, 2, 3, 4, 5)
+        // val testRDD = sc.parallelize(data)
+        // testRDD.saveToRiak("kv_bucket_a")
 
         sc.stop()
     }
