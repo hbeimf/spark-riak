d2c8526ffe21a15247c3b639fece3970ccea194d
maofeng <maofeng@jrq.com>
2017-05-27 13:50:56 +0800

    dataframe

--- src/main/scala/SparkRiak.scala
+++ src/main/scala/SparkRiak.scala
@@ -1,58 +1,5 @@
 package SparkRiak
 
-// import org.apache.spark.{SparkContext, SparkConf}
-// import com.basho.riak.spark._
-
-
-// // https://github.com/basho/spark-riak-connector/blob/master/docs/using-connector.md#configuration-of-spark-context
-
-// object SparkRiak {
-//     def main(args: Array[String]): Unit = {
-//         val appName = "SparkRiak"
-
-//         val conf = new SparkConf()
-//         .setAppName(appName)
-//         .set("spark.riak.connection.host", "127.0.0.1:8087")
-//         .set("spark.riak.connections.min", "20")
-//         .set("spark.riak.connections.max", "50")
-
-//         val sc = new SparkContext("spark://127.0.0.1:7077", appName, conf)
-
-//         val rdd = sc.riakBucket[String]("bucket1").queryAll()
-//         rdd.collect().foreach(println)
-
-//         println("spark riak!!!")
-
-
-//         // val data = Array(1, 2, 3, 4, 5)
-//         // val testRDD = sc.parallelize(data)
-//         // testRDD.saveToRiak("kv_bucket_a")
-
-//         sc.stop()
-//     }
-// }
-
-
-
-/**
- * Copyright (c) 2015 Basho Technologies, Inc.
- *
- * This file is provided to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License.  You may obtain
- * a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-// package com.basho.riak.spark.examples.dataframes
-
 import com.basho.riak.client.core.query.indexes.LongIntIndex
 import com.basho.riak.client.core.query.Namespace
 import com.basho.riak.spark._
